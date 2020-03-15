f561e9211be6d47c1bad53ca71e254c6e30d4671
BahadirOkur <fikretbahadirokur@gmail.com>
2020-03-15 15:50:57 -0700

    conflict

--- src/thirdGithubPractice/LosAngeles.java
+++ src/thirdGithubPractice/LosAngeles.java
@@ -5,7 +5,7 @@ public class LosAngeles {
 	public static void main(String[] args) {
 		
 		System.out.println("Private Practice");
-		System.out.println("Little update");
+		System.out.println("Some changes");
 		System.out.println("This is an update from github website");
 	}
 
