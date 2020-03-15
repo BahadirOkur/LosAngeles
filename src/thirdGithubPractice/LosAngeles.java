945f899cdffba5ec6ced00dc746f62201ee417de
BahadirOkur <62161875+BahadirOkur@users.noreply.github.com>
2020-03-15 15:49:10 -0700

    merge conflict

--- src/thirdGithubPractice/LosAngeles.java
+++ src/thirdGithubPractice/LosAngeles.java
@@ -5,8 +5,9 @@ public class LosAngeles {
 	public static void main(String[] args) {
 		
 		System.out.println("Private Practice");
-		System.out.println("Little update");
+		System.out.println("lots of updates");
 		System.out.println("This is an update from github website");
+		
 	}
 
 }
